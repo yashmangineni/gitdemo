1a80d8db543c25f51d056e7338fee667a5e460ff
[200~yashmangineni~ <yaswanthmangineni@gmail.com>
2024-08-05 09:47:44 +0530

    yash

---
+++ yash.java
@@ -0,0 +1,15 @@
+class prgm7{
+public static void main(String args[]){
+int sp=100,cp=200;
+if(sp>cp)
+{
+int p=sp-cp;
+System.out.println(p);
+}
+else
+{
+int loss=cp-sp;
+System.out.println(loss);
+}
+}}
+
